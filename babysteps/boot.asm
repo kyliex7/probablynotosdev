4072bcd7cc0b02146353ae0f8b241186051ce21e
kyliex7 <216264999+kyliex7@users.noreply.github.com>
2026-03-31 20:21:12 +0530

    add dot

--- babysteps/boot.asm
+++ babysteps/boot.asm
@@ -18,27 +18,28 @@ loader:
   push bx
   pop ax
   pop bx
+  ;;
 
   cld
   mov si, msg
   call bios_print
 
-hang:
-  jmp hang
+.hang:
+  jmp .hang
 
 bios_print:
   lodsb
   or al, al
-  jz done
+  jz .done
 
   mov ah, 0x0e
   mov bh, 0
   int 0x10
   jmp bios_print
-done:
+.done:
   ret
 
-msg: db "hello from this", 0xd, 0xa, 0
+msg: db "hello from this side", 0xd, 0xa, 0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
